3401fbd7566ae5a03c9162ed3170ea18c443d1a7
Badmajor <vtk88@mail.ru>
2021-09-29 14:13:25 +0300

    0.8.9 отчет админу о запуске и закритии сквада

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,23 +1,13 @@
-import logging
+
 
 from aiogram import Dispatcher
 
-from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(int(admin), "Бот Запущен")
-
-        except Exception as err:
-            logging.exception(err)
+    await dp.bot.send_message(530291098, "Бот Запущен")
 
 
 async def completed_squad(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(int(admin), "Squad закрыт")
+    await dp.bot.send_message(530291098, "Squad закрыт")
 
-        except Exception as err:
-            logging.exception(err)
