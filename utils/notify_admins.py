0fa1e514f1c644c406853134c25deeeafcbc03e2
Badmajor <vtk88@mail.ru>
2021-09-29 13:54:09 +0300

    0.8.9 отчет админу о запуске

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,8 +1,12 @@
-
+import logging
 from aiogram import Dispatcher
 from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
-        await dp.bot.send_message(admin, "Бот Запущен")
+        try:
+            await dp.bot.send_message(admin, "Бот Запущен")
+
+        except Exception as err:
+            logging.exception(err)
